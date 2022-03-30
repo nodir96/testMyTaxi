564ac42bde5fe59a8ffbf7dbdf03e696a560b1a4
Rahmatullayev Nodir <nodir96.07@gmail.com>
2022-03-30 20:47:15 +0500

    google map is ready

--- app/src/main/java/uz/cap/testmytaxi/MainActivity.kt
+++ app/src/main/java/uz/cap/testmytaxi/MainActivity.kt
@@ -1,13 +1,27 @@
 package uz.cap.testmytaxi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment as SupportMapFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val fragment: SupportMapFragment =
+            supportFragmentManager.findFragmentById(R.id.maps) as SupportMapFragment
+
+        fragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
 
     }
+
+
 }
